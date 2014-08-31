9b8d508cb488e015f348b7d05904c2d5269bd3c8
John Otander <johnotander@gmail.com>
2014-08-31 15:15:39 -0600

    Update the url.

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,7 +1,7 @@
 import DS from "ember-data";
 
 var ApplicationAdapter = DS.ActiveModelAdapter.extend({
-  host: 'http://lit-hollows-1671.herokuapp.com',
+  host: 'http://api.firstchair.io',
 
   buildURL: function() {
     var base;
