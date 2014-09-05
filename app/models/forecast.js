023decab0332c8b465627765588de4c1f32aac87
John Otander <johnotander@gmail.com>
2014-09-04 22:00:32 -0600

    Add days computed property to forecast.

--- app/models/forecast.js
+++ app/models/forecast.js
@@ -7,5 +7,18 @@ export default DS.Model.extend({
   highs: DS.attr('array'),
   lows: DS.attr('array'),
 
-  resort: DS.belongsTo('resort')
+  resort: DS.belongsTo('resort'),
+
+  days: function() {
+    var weather = this.get('weather');
+    var highs = this.get('highs');
+    var lows = this.get('lows')
+    return this.get('weather').map(function(currWeather, index) {
+      return {
+        weather: currWeather,
+        high: highs[index],
+        low: lows[index]
+      };
+    });
+  }.property('weather', 'highs', 'lows')
 });
