544c57b46d3286d1994f4df7573285d4a30e4da7
John Otander <johnotander@gmail.com>
2014-09-04 21:20:24 -0600

    Display raw data for the forecast.

--- app/models/forecast.js
+++ app/models/forecast.js
@@ -3,7 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   startsAt: DS.attr('string'),
   endsAt: DS.attr('string'),
-  weather: DS.attr('string'),
+  weather: DS.attr('array'),
   highs: DS.attr('array'),
   lows: DS.attr('array'),
 
