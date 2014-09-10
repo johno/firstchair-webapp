3722339fb110eb1a793c10ee18c6672ca0844d86
John Otander <johnotander@gmail.com>
2014-09-09 22:13:33 -0600

    Add print statement to possibly detect if router is being hit.

--- app/routes/forecast.js
+++ app/routes/forecast.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(params) {
+    console.log('hello');
     return this.store.find('forecast', params.forecast_id);
   }
 });
