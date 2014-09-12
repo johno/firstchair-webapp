bb25560014aefb019ce0d30e8411547bca923498
John Otander <johnotander@gmail.com>
2014-09-12 10:42:57 -0600

    Add activemodel adapter addon.

--- app/app.js
+++ app/app.js
@@ -9,6 +9,8 @@ var App = Ember.Application.extend({
   Resolver: Resolver
 });
 
+Ember.ENV.ACTIVE_MODEL_API_URL = 'http://api.firstchair.io';
+
 loadInitializers(App, 'firstchair');
 
 export default App;
