6dcf6a4a6455574866f3dc2a864d79d9296a817e
John Otander <johnotander@gmail.com>
2014-10-09 08:48:24 -0600

    Nuke ga for now, finish upgrading.

--- app/helpers/google-analytics.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-function googleAnalytics(value) {
-    return new Handlebars.SafeString("<script type='text/javascript'>var _gaq = _gaq || [];_gaq.push(['_setAccount', '" + value + "']);(function() {var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);})();</script>");
-}
-
-export { googleAnalytics };
-
-export default Ember.Handlebars.makeBoundHelper(googleAnalytics);
--- tests/unit/helpers/google-analytics-test.js
+++
@@ -1,9 +0,0 @@
-import { googleAnalytics } from 'firstchair/helpers/google-analytics';
-
-module('GoogleAnalyticsHelper');
-
-// Replace this with your real tests.
-test('it works', function() {
-  var result = googleAnalytics(42);
-  ok(result);
-});
